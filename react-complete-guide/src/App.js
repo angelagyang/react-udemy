e79ad7a4f27b7e37ec7815c3ca477d482f17d8e4
Angela Yang <agyang@andrew.cmu.edu>
2021-01-06 16:42:23 -0500

    Add state examples

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,11 +3,30 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = { 
+    persons: [
+      {name: "Angela", age: 21}, 
+      {name: "Andy", age: 19}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // Don't do this! this.state.persons[1] = "Angela Yang";
+    // console.log('clicked');
+    this.setState({persons: [
+      {name: "Angela", age: 22}, 
+      {name: "Andy", age: 20}
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person></Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age ={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}>My hobbies: gaming</Person>
+
       </div>
     );
 
